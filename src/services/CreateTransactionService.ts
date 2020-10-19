9ec85fa0a8f22a97be00dd1af56621d81a6d6ee2
Giovani Silva <giovani.silva@live.com>
2020-10-19 14:38:59 -0300

    desafio concluido

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('There is not enough balance to make the outcome');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
