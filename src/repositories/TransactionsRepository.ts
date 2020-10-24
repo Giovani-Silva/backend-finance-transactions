f569ee02f02dee30cc6fd1f64f100facb4d0fd69
unknown <giovani.silva@live.com>
2020-10-24 00:44:32 -0300

    list transactions with balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,24 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (acc, transaction) => {
+        acc[transaction.type] += Number(transaction.value);
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
